71c7558cde44eea61fd5d2a38d1730874597b864
lucasciancio <lucas.ciancio2@gmail.com>
2023-11-06 12:33:27 -0300

    main resuelto

--- main.c
+++ main.c
@@ -6,11 +6,23 @@
 #define MAX_LEXEMA 15
 
 
+struct posicion {
+    int linea;
+    int columna;
+};
+
+struct posicion posicionActual = {0,0};
 
 // PROTOTIPOS
 
 void inicializarVector (int largo, char vector[]);
-
+int ignorarEntrada(char entrada);
+void agregarAlLexema(char entrada, int maxLexema, char lexema[]);
+char* obtenerToken(int estado);
+void imprimirEncabezado();
+void imprimirToken(char token[], char lexema[]);
+void actualizarPosicion(struct posicion* posicionActual, char entrada);
+void retrocederPosicion(struct posicion* posicionActual, char entrada);
 
 
 // MAIN
@@ -22,9 +34,23 @@ int main (void) {
 
     inicializarVector(maxLexema, lexema);
 
-    int entrada;
+    int entrada, estado;
 
-    while(entrada = getc(stdin)) {
+    imprimirEncabezado();
+
+    while((entrada = getc(stdin)) != EOF) {
+
+        if(!ignorarEntrada(entrada)) {
+
+            inicializarVector(maxLexema, lexema);
+
+            agregarALexema(entrada, maxLexema, lexema);
+
+            estado = AFD(maxLexema, lexema);
+
+            imprimirToken(obtenerToken(estado), lexema);
+        
+        } else { actualizarPosicion(&posicionActual, entrada); }
 
     }
 
@@ -34,8 +60,91 @@ int main (void) {
 
 // FUNCIONES
 
+
+int ignorarEntrada(char entrada) {
+    return entrada == '\n' || entrada == '\r' || entrada == ' ';
+}
+
+// (1+1) *2\n3/4\n2\n3
+
+
+void agregarAlLexema(char entrada, int maxLexema, char lexema[]) {
+
+    int i = 0;
+
+    if(isdigit(entrada)) {
+        
+        do {
+            actualizarPosicion(&posicionActual, entrada);
+            lexema[i] = entrada;
+            i++;
+        } while ((entrada = getc(stdin) != EOF) && (isdigit(entrada)) && (i < maxLexema));
+
+        ungetc(entrada, stdin);
+        retrocederPosicion(&posicionActual, entrada);
+
+    } else {
+        if(!ignorarEntrada(entrada)) { lexema[i] = entrada; }
+    }
+
+    return lexema;
+}
+
+char* obtenerToken(int estado) {
+    char token[30+1];
+    inicializarVector(30, token);
+
+    switch(estado) {
+        case 1: 
+            strcpy(token, "operador");
+            break;
+        case 2:
+            strcpy(token, "numero");
+            break;
+        default: strcpy(token, "desconocido [Ln ..., Col ...]");
+    }
+
+    return token;
+}
+
+/*
+    PALABRA | TOKEN
+    --------|---------
+    (       | operador
+    --------|-------------
+    1       |
+
+    PALABRA | TOKEN
+    (       | operador
+    1       | numero
+
+
+*/
+
+
+void imprimirEncabezado() {
+    printf("%-15s | %-30s\n", "PALABRA", "TOKEN");
+}
+
+void imprimirToken(char token[], char lexema[]) {
+    printf("%-15s | %-30s\n", lexema, token);
+}
+
+
 void inicializarVector (int largo, char vector[]) {
     for(int i = 0; i < largo; i++) {
         vector[i] = ' ';
     }
+}
+
+
+void actualizarPosicion(struct posicion* posicionActual, char entrada) {
+    if (entrada == '\n') {
+        (*posicionActual).linea++;
+        (*posicionActual).columna = 0;
+    } else { (*posicionActual).columna ++; }
+}
+
+void retrocederPosicion(struct posicion* posicionActual, char entrada) {
+    (*posicionActual).columna--;
 }
---
+++ reconocedor.h
@@ -0,0 +1,6 @@
+#ifndef RECONOCEDOR_H
+#define RECONOCEDOR_H
+
+int AFD(int maxLexema, char lexema[]);
+
+#endif
